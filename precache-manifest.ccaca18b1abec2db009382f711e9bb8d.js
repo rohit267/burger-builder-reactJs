cef2765a39a0e2d2d73c075025733909bfb8fd11
Rohit Mahto <rohitmahto267@gmail.com>
2020-02-01 16:45:52 +0530

    Updates

---
+++ precache-manifest.ccaca18b1abec2db009382f711e9bb8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975d4084aa033c59b3dfa76dd61f4add",
+    "url": "/burger-builder-reactJs/index.html"
+  },
+  {
+    "revision": "f00b4595fabd22790f72",
+    "url": "/burger-builder-reactJs/static/css/main.e3e7925c.chunk.css"
+  },
+  {
+    "revision": "2a2b77abbf38c6625c59",
+    "url": "/burger-builder-reactJs/static/js/2.f5e60c04.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/burger-builder-reactJs/static/js/2.f5e60c04.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f00b4595fabd22790f72",
+    "url": "/burger-builder-reactJs/static/js/main.474f66e1.chunk.js"
+  },
+  {
+    "revision": "5270b676b9761813926b",
+    "url": "/burger-builder-reactJs/static/js/runtime-main.69323a76.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder-reactJs/static/media/burger-logo.b8503d26.png"
+  }
+]);
